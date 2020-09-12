139675bcf6e584008904db04dc2f1045ada95e0e
abhi1302-js <69789359+abhi1302-js@users.noreply.github.com>
2020-09-12 13:43:30 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var myname = document.querySelector(".name");
+var grade = document.querySelector(".class");
+var section = document.querySelector(".section");
+function test() {
+   document.textcontent("I "+ myname.value +" of "+ grade.value + " " + section.value + " agree's to the terms and conditions of this website.")
+    
+};
